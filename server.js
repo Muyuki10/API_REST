b13e1a8bcd5fcb7d245c947c421a5099e148c84b
Muyuki10 <muyuki2010@gmail.com>
2026-08-14 14:14:13 -0300

    tratando 404

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const { router } = require('./src/routes/TesteRoutes');
+const path = require('path')
 const port = 3000;
 
 const app = express()
 app.use(express.json())
 
+
+
 app.use( router )
 
+//tratando 404 com html
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname, './src/view/404.html'))
+})
 
 app.listen(port, ()=>{
     console.log("http://localhost:3000")
