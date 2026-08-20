aabc2d504d29802957712db1f28931b23159cfec
Muyuki10 <muyuki2010@gmail.com>
2026-08-20 18:11:03 -0300

    testing

--- src/models/TesteModels.js
+++ src/models/TesteModels.js
@@ -1,12 +1,49 @@
+const sql = require('mysql2')
+
+const connection = sql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'',//digite sua senha aqui
+    database: 'user'
+})
+
+connection.connect((erro) => {
+    if(erro){
+        console.log(erro)
+
+    }else{
+        console.log("conectado com sucesso")
+    }
+})
+
 const users = [{id: 1,nome:"murilo"}]; 
 let id = 0
 
 function listarM(){
+     const query = "SELECT * FROM users"
+    connection.query(query,(erro,resultado)=>{
+        if (erro){
+            console.log(erro)
+        }
+        else {
+            console.log(resultado)
+        }
+    })
     return users
 }
 function criarM(nome){
+
+    const query = "INSERT INTO users(nome) VALUES (?);"
     id += 1
     users.push({nome})
+    connection.query(query,nome ,(erro,resultado)=>{
+        if (erro){
+            console.log(erro)
+        }
+        else {
+            console.log("criado com sucesso")
+        }
+    })
 }
 function atualizarM(id,nomeN){
     const onde = users.findIndex(i => i.id === id)
